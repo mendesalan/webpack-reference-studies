8f57c348886766c970bcf3bc789494d8ed96e4a7
Alan <alanvianamendes@outlook.com>
2018-02-28 20:08:44 -0300

    simplest webpack config with a file

---
+++ 01-Zero-Config/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundled.main.js'
+  }
+
+  // module: {
+  //   loaders: [
+  //       { test: /\.js$/, loader: 'buble-loader' }
+  //   ]
+  // }
+
+};
