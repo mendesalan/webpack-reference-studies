2cbc908e92ef9a3b722ed3101a1d6e4e9e1727c9
Alan <alanvianamendes@outlook.com>
2018-03-01 19:26:02 -0300

    webpack-babel running es-2015 for all browsers

---
+++ 02-webpack-babel/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundled.main.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+
+        ]
+    }
+
+};
