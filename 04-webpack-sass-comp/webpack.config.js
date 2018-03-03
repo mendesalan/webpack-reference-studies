e9915b1acb9adcbf5bbbe7baec7a5b1fd151690b
Alan <alanvianamendes@outlook.com>
2018-03-02 21:05:42 -0300

    compile scss and extract and minimize to a separetad file

---
+++ 04-webpack-sass-comp/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var envMode = process.env.NODE_ENV
+var inProduction = (process.env.NODE_ENV === 'production')
+
+module.exports = {
+    mode: envMode,
+
+    entry: {
+        app: [
+            './src/main.js',
+            './src/main.scss'
+        ]
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundled.main.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+
+    plugins: [
+
+        new ExtractTextPlugin("styles.css"),
+
+        new webpack.LoaderOptionsPlugin({
+
+            minimize: inProduction
+
+        })
+    ]
+
+};
+
+if (inProduction) {
+
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
